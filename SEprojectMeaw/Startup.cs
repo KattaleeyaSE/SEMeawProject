8d8c75119aa50b207ea8e8dfc45134f1dabb0d03
KattaleeyaSE <Kattaleeya_k@cmu.ac.th>
2015-11-15 16:29:12 +0700

    Version1.0

---
+++ SEprojectMeaw/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SEprojectMeaw.Startup))]
+namespace SEprojectMeaw
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
